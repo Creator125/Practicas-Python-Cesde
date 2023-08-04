418555a04b087e19eac1c49060540e280f12a962
Creator125 <obedagualimpia1@gmail.com>
2023-08-04 11:36:15 -0500

    Terminado practica

--- Ejemplo1/hola.py
+++ Ejemplo1/hola.py
@@ -1,1 +1,19 @@
-print("Esto esrá divertido")
+#print("Esto esrá divertido")
+
+nombreUsuario = "Creator125"
+edad = 22
+estatura = 1.80
+esDivertido = True
+comidaFavorita = "Pollo"
+ciudad = input("Ingrese tu ciudad: ")
+salarioMensual= input("Ingrese tu salario: ")
+deudasMes = input("Ingrese la deudas pagar: ")
+
+'''
+print(f"Hola {nombreUsuario} tu edad es {edad}")
+print(f"Ho tu comidad favirita es {comidaFavorita} y vives en {ciudad}")
+'''
+
+sumaTotal = salarioMensual - deudasMes
+
+print(f"Despues de pagar deudas tu salario mensual es {deudasMes}")
