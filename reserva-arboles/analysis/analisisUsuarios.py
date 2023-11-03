00ebede153519c6fe7d9527625d2c028bda6ce66
Creator125 <85368990+Creator125@users.noreply.github.com>
2023-11-03 12:09:31 -0500

    reserva-arboles/: Se agregaron generadores de archivos

--- reserva-arboles/analysis/analisisUsuarios.py
+++ reserva-arboles/analysis/analisisUsuarios.py
@@ -1,5 +1,14 @@
 from data.ListaUsuarios import usuarios
 from helpers.crearCSV import crearCSVUsuarios
+from helpers.crearHTML import crearTabla
+import pandas as pd
 
 #Usar la funcion crearSCVUsuarios
-crearCSVUsuarios(usuarios, "bdUsuarions.csv")
+crearCSVUsuarios(usuarios, "bdUsuarios.csv")
+
+#creando un dataframe desde una fuente CSV
+dataFrameUsuarios = pd.read_csv('data/bdUsuarios.csv')
+print(dataFrameUsuarios)
+
+#convertir el DF en TABLA HTML
+crearTabla(dataFrameUsuarios,'usuarios')
---
+++ reserva-arboles/helpers/crearHTML.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+def crearTabla(dataframe,nombre):
+
+    archivoHTML=dataframe.to_html()
+    rutaArchivo=f"tables/{nombre}.html"
+
+    encabezado=''' 
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <title>Tablas</title>
+                    <link rel="stylesheet" href="/assets/styles/bootstrap.css">
+                </head>
+            </html>
+        '''
+    
+    sopa=BeautifulSoup(archivoHTML,'html.parser')
+
+    for tr in sopa.find_all('tr'):
+        tr.attrs.pop('style',None)
+
+    archivoHTML = str(sopa) 
+    archivoHTML = archivoHTML.replace('<table border="1" class="dataframe">','<table class="table table-striped">')
+
+    with open(rutaArchivo,"w") as archivo:
+        archivo.write(f"{encabezado}\n{archivoHTML}\n</body>\n</html>")
